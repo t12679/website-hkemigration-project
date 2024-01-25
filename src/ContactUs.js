9a909701df7cbe94309827d243e3dfd43229d686
lihaoting <1852595418@qq.com>
2024-01-25 14:49:04 +0800

    added contact info

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import useContentful from './useContentful';
+import ReactMarkdown from 'react-markdown';
+import './ContactUs.css';
 
-function ContactUs() {
+
+function ContactUs({currentLanguage}) {
+
+  const { data: Pagedata, isLoading, error } = useContentful('contactUs', currentLanguage);
+
+
+  if (isLoading) {
+    return <p></p>;
+  }
+
+  if (error) {
+      return <div>Error: {error.message}</div>; // render some error state
+    }
+
+  
+      const entry = Pagedata ? Pagedata[0] : null;
+
+
   return (
-    <div>
-      <h2>Comming soon...</h2>
+    <div className='ContactUs'>
+      <div className='Contact-info'>
+        <ReactMarkdown>{entry && entry.fields.heading}</ReactMarkdown>
+          <ReactMarkdown>{entry && entry.fields.contact1}</ReactMarkdown>
+          <ReactMarkdown>{entry && entry.fields.contact2}</ReactMarkdown>
+          <ReactMarkdown>{entry && entry.fields.contact3}</ReactMarkdown>
+        </div>
     </div>
   );
 }
